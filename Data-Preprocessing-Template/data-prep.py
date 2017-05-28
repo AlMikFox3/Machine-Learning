a2bae731caad6675611c802f7c9a15b70ad5c83c
AlMikFox3 <sasmit1997@outlook.com>
2017-05-28 21:22:02 +0530

    Handling Categorical Data

--- Data-Preprocessing-Template/data-prep.py
+++ Data-Preprocessing-Template/data-prep.py
@@ -14,3 +14,12 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy="mean", axis = 0)
 imputer = imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+#Categorical Data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+label_enc_x = LabelEncoder()
+x[:, 0] = label_enc_x.fit_transform(x[:, 0])
+onehotenc_x = OneHotEncoder(categorical_features = [0])
+x = onehotenc_x.fit_transform(x).toarray()
+label_enc_y = LabelEncoder()
+y = label_enc_y.fit_transform(y)
