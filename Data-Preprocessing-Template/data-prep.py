28bf21b65a0cb3215094be27bddf995707e5040c
AlMikFox3 <sasmit1997@outlook.com>
2017-05-28 22:04:53 +0530

    Feature Scaling

--- Data-Preprocessing-Template/data-prep.py
+++ Data-Preprocessing-Template/data-prep.py
@@ -27,3 +27,9 @@ y = label_enc_y.fit_transform(y)
 #Splitting into train and test set.
 from sklearn.cross_validation import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.30, random_state = 1)
+
+#Feature Scaling - Normalization & Standardization
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test) #no fit as the sc_x object is already fit from the previous step
